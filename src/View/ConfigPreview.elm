e1dcc7272acd6d62e80c1a539a37a7f374a849b9
Jan Hrcek <jhrcek@redhat.com>
2017-08-15 13:12:27 +0200

    Add strategy file download link

--- src/View/ConfigPreview.elm
+++ src/View/ConfigPreview.elm
@@ -1,18 +1,31 @@
 module View.ConfigPreview exposing (view)
 
 import Data.Strategy as Strategy
-import Html exposing (Html, textarea)
-import Html.Attributes exposing (cols, readonly, rows, style, value)
+import Html exposing (Html, a, div, text, textarea)
+import Html.Attributes exposing (cols, downloadAs, href, readonly, rows, style, value, width)
 import Types exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-    textarea
-        [ rows 50
-        , cols 80
-        , value (Strategy.renderParsedStrategy model)
-        , readonly True
-        , style [ ( "position", "fixed" ), ( "right", "0" ), ( "top", "0" ), ( "width", "50%" ) ]
-        ]
-        []
+    let
+        strategyString =
+            Strategy.renderParsedStrategy model
+    in
+        div [ style [ ( "position", "fixed" ), ( "right", "0" ), ( "top", "0" ), ( "width", "50%" ) ] ]
+            [ textarea
+                [ rows 50
+                , style [ ( "width", "100%" ) ]
+                , value strategyString
+                , readonly True
+                ]
+                []
+            , div []
+                [ text "Download "
+                , a
+                    [ href <| "data:text/plain;charset=utf-8," ++ strategyString
+                    , downloadAs <| "strategy.txt"
+                    ]
+                    [ text "strategy file" ]
+                ]
+            ]
