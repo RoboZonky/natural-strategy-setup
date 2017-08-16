27a791037871f889c525b9d32b3d9c8ec634045b
Jan Hrcek <jhrcek@redhat.com>
2017-08-16 10:34:14 +0200

    Model PortfolioShare

--- src/Data/PortfolioShare.elm
+++ src/Data/PortfolioShare.elm
@@ -1,5 +1,32 @@
-module Data.PortfolioShare exposing (..)
+module Data.PortfolioShare
+    exposing
+        ( PortfolioShare(..)
+        , Share(..)
+        , renderPortfoliShare
+        )
+
+import Data.Rating as Rating exposing (Rating)
 
 
 type PortfolioShare
-    = PortfolioShare
+    = PortfolioShare Rating Share
+
+
+type Share
+    = Exact Int
+    | Range Int Int
+
+
+renderPortfoliShare : PortfolioShare -> String
+renderPortfoliShare (PortfolioShare rating share) =
+    "Prostředky v ratingu " ++ Rating.ratingToString rating ++ " tvoří " ++ renderShare share ++ " % aktuální zůstatkové částky."
+
+
+renderShare : Share -> String
+renderShare share =
+    case share of
+        Exact percentage ->
+            toString percentage
+
+        Range minPercent maxPercent ->
+            toString minPercent ++ " až " ++ toString maxPercent
