d9e789cf905c5b67a73fe037fb8f27b789ef37fb
Jan Hrček <jhrcek@redhat.com>
2017-09-16 09:34:43 +0200

    Minify elm.js file while deploying it

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@
 DEPLOYMENT_DIR=../../jhrcek/jhrcek.github.io/natural-strategy-setup
 rm --recursive --force $DEPLOYMENT_DIR
 cp --recursive dist $DEPLOYMENT_DIR
+
+echo "Minifying js file"
+rm $DEPLOYMENT_DIR/js/elm.js
+uglifyjs dist/js/elm.js --compress --mangle --output $DEPLOYMENT_DIR/js/elm.js
