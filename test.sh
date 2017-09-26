f148240dc0ce0b56a184da2b8e6da2202efd189b
Jan Hrcek <jhrcek@redhat.com>
2017-09-26 12:17:29 +0200

    Automatically download chromedriver binary for tests if not exists

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Download chromedriver binary if it doesn't exist
+if [ ! -f "tests/chromedriver" ]; then
+  echo "chromedriver binary required by tests doesn't exist. Attemping download.."
+  wget --quiet --directory-prefix tests https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip
+  unzip tests/chromedriver_linux64.zip -d tests
+fi
+
 elm make src/Test/TestApp.elm --warn --output tests/target/testApp.html
 cd tests || exit
 mvn test -Dmake.elmMakeExecutable=/usr/bin/elm-make
