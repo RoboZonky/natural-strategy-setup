520a68040a438acbadf4004e9c3e0e49af9c222c
Jan Hrček <jhrcek@redhat.com>
2017-10-19 17:40:53 +0200

    Rename selenium tests folder to integration-tests

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+TESTS_DIR=integration-tests
 # Download chromedriver binary if it doesn't exist
-if [ ! -f "tests/chromedriver" ]; then
+if [ ! -f "${TESTS_DIR}/chromedriver" ]; then
   echo "chromedriver binary required by tests doesn't exist. Attemping download.."
-  wget --quiet --directory-prefix tests https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip
-  unzip tests/chromedriver_linux64.zip -d tests
+  wget --quiet --directory-prefix ${TESTS_DIR} https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip
+  unzip ${TESTS_DIR}/chromedriver_linux64.zip -d ${TESTS_DIR}
 fi
 
-elm make src/Test/TestApp.elm --warn --output tests/target/testApp.html
-cd tests || exit
+elm make src/Test/TestApp.elm --warn --output ${TESTS_DIR}/target/testApp.html
+cd ${TESTS_DIR} || exit
 mvn test -Dmake.elmMakeExecutable=/usr/bin/elm-make
