24d00a8d4c631708b652fd65801fb1c3129b6867
Jan Hrček <jhrcek@redhat.com>
2017-10-20 13:34:22 +0200

    Add unit test execution to test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+set -e # Exit immediately when some command fails
 
+# Run unit tests
+elm test
+
+# Run Integration tests
 TESTS_DIR=integration-tests
 # Download chromedriver binary if it doesn't exist
 if [ ! -f "${TESTS_DIR}/chromedriver" ]; then
-  echo "chromedriver binary required by tests doesn't exist. Attemping download.."
-  wget --quiet --directory-prefix ${TESTS_DIR} https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip
+  echo "chromedriver binary required by tests doesn't exist. Attempting download.."
+  wget --quiet --directory-prefix ${TESTS_DIR} https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip
   unzip ${TESTS_DIR}/chromedriver_linux64.zip -d ${TESTS_DIR}
 fi
 
 elm make src/Test/TestApp.elm --warn --output ${TESTS_DIR}/target/testApp.html
-cd ${TESTS_DIR} || exit
+cd ${TESTS_DIR}
 mvn test -Dmake.elmMakeExecutable=/usr/bin/elm-make
