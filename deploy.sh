2208ad91118fd01e4b410a8576d9330011a5d76d
Jan Hrček <jhrcek@redhat.com>
2018-10-14 11:47:33 +0200

    Use better command for minifying resulting elm file
    This reduces the size of the minified elm file from 430 KB to 312 KB.

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ fi
 
 echo "Minifying js file"
 rm $DEPLOYMENT_DIR/js/elm.js
-uglifyjs dist/js/elm.js --compress --mangle --output $DEPLOYMENT_DIR/js/elm.js
+uglifyjs dist/js/elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$DEPLOYMENT_DIR/js/elm.js
