9f5ce42cc2bf2360c3a0b4c58a6dffc89481101e
Jan Hrček <honza.hrk@gmail.com>
2020-05-17 07:35:51 +0200

    Format via ormolu, bump to latest lts and elm-review versions

--- shake.hs
+++ shake.hs
@@ -1,106 +1,99 @@
 #!/usr/bin/env stack
 {- stack script
-   --resolver lts-15.10
+   --resolver lts-15.12
    --package process
    --package shake
 -}
-import           Development.Shake
-import           Development.Shake.FilePath
-import qualified System.Process             as Process
+
+import Development.Shake
+import Development.Shake.FilePath
+import qualified System.Process as Process
 
 -- TODO maybe check all system dependencies are present: elm, elm-live, java, mvn, sed, uglify-js, unzip, warp, wget
 
 main :: IO ()
 main = shakeArgs options $ do
-    want [elmDist]
+  want [elmDist]
 
-    elmDist %> \out -> do
-        StdoutTrim elmFiles <- command [] "git" ["ls-files", "src"]
-        need $ lines elmFiles
+  elmDist %> \out -> do
+    StdoutTrim elmFiles <- command [] "git" ["ls-files", "src"]
+    need $ lines elmFiles
 
-        cmd_ "elm make src/Main.elm --optimize --output" out
-        -- Hardcode current commit hash into distribution
-        -- to make it easy to link generated strategy configs to specific version of this tool
-        StdoutTrim commitHash <- command [] "git" ["rev-parse", "--short", "HEAD"]
-        cmd_ ("sed -i s/COMMIT_HASH_PLACEHOLDER/" <> commitHash <> "/")  out
-    
-    testApp %> \_ -> do
-        cmd_ "elm make src/Test/TestApp.elm --optimize --output" testApp
+    cmd_ "elm make src/Main.elm --optimize --output" out
+    -- Hardcode current commit hash into distribution
+    -- to make it easy to link generated strategy configs to specific version of this tool
+    StdoutTrim commitHash <- command [] "git" ["rev-parse", "--short", "HEAD"]
+    cmd_ ("sed -i s/COMMIT_HASH_PLACEHOLDER/" <> commitHash <> "/") out
 
-    chromedriverZip %> \_ -> do
-        StdoutTrim latestVersion <- command [] "curl" ["http://chromedriver.storage.googleapis.com/LATEST_RELEASE"]
-        cmd_ "wget" "--quiet" "--directory-prefix" it ("http://chromedriver.storage.googleapis.com/" <> latestVersion <> "/chromedriver_linux64.zip")
+  testApp %> \_ -> do
+    cmd_ "elm make src/Test/TestApp.elm --optimize --output" testApp
 
-    chromedriver %> \_ -> do
-        need [chromedriverZip]
-        cmd_ "unzip" (it </> "chromedriver_linux64.zip") "-d" it
+  chromedriverZip %> \_ -> do
+    StdoutTrim latestVersion <- command [] "curl" ["http://chromedriver.storage.googleapis.com/LATEST_RELEASE"]
+    cmd_ "wget" "--quiet" "--directory-prefix" it ("http://chromedriver.storage.googleapis.com/" <> latestVersion <> "/chromedriver_linux64.zip")
 
-    phony minify $ do
-        need [elmDist]
-        withTempFile $ \minifiedTemp -> do
-            cmd_ "uglifyjs" [elmDist, "--compress", "pure_funcs=\"F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9\",pure_getters,keep_fargs=false,unsafe_comps,unsafe", "--output", minifiedTemp]
-            cmd_ "uglifyjs" [minifiedTemp, "--mangle", "--output" , elmDist]
-    
-    phony "dev-server" $ do
-        cmd_ "elm-live --dir dist --open -- src/Main.elm --output" elmDist
+  chromedriver %> \_ -> do
+    need [chromedriverZip]
+    cmd_ "unzip" (it </> "chromedriver_linux64.zip") "-d" it
 
-    phony "test" $ do
-        cmd_ "elm-test"
-    
-    phony "itest" $ do
-        need ["test", elmDist, chromedriver, testApp]
-        let startWarp = fromProcess <$> cmd "warp --docroot dist"
-        actionBracket startWarp Process.terminateProcess $ \_ -> do
-          mvn "test"
+  phony minify $ do
+    need [elmDist]
+    withTempFile $ \minifiedTemp -> do
+      cmd_ "uglifyjs" [elmDist, "--compress", "pure_funcs=\"F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9\",pure_getters,keep_fargs=false,unsafe_comps,unsafe", "--output", minifiedTemp]
+      cmd_ "uglifyjs" [minifiedTemp, "--mangle", "--output", elmDist]
 
-    phony "deploy-stage" $ do
-        deploy "/home/janhrcek/Devel/github.com/jhrcek/jhrcek.github.io/natural-strategy-setup"
+  phony "dev-server" $ do
+    cmd_ "elm-live --dir dist --open -- src/Main.elm --output" elmDist
 
-    phony "deploy-prod" $ do
-        deploy "/home/janhrcek/Devel/github.com/RoboZonky/robozonky.github.io/konfigurace-strategie"
-        
-    phony "clean" $ do
-        cmd_ "rm" ["-rf", "elm-stuff", elmDist]
-        mvn "clean"
-        
+  phony "test" $ do
+    cmd_ "elm-test"
+
+  phony "itest" $ do
+    need ["test", elmDist, chromedriver, testApp]
+    let startWarp = fromProcess <$> cmd "warp --docroot dist"
+    actionBracket startWarp Process.terminateProcess $ \_ -> do
+      mvn "test"
+
+  phony "deploy-stage" $ do
+    deploy "/home/jhrcek/Devel/github.com/jhrcek/jhrcek.github.io/natural-strategy-setup"
+
+  phony "deploy-prod" $ do
+    deploy "/home/jhrcek/Devel/github.com/RoboZonky/robozonky.github.io/konfigurace-strategie"
+
+  phony "clean" $ do
+    cmd_ "rm" ["-rf", "elm-stuff", elmDist]
+    mvn "clean"
 
 mvn :: String -> Action ()
 mvn goal = command_ [Cwd it] "mvn" [goal]
 
-
 deploy :: FilePath -> Action ()
 deploy targetDir = do
-    need [minify]
-    liftIO $ removeFiles targetDir ["*"]
-    cmd_ "cp --recursive dist/." targetDir
-
+  need [minify]
+  liftIO $ removeFiles targetDir ["*"]
+  cmd_ "cp --recursive dist/." targetDir
 
 options :: ShakeOptions
-options = shakeOptions
-    { shakeVerbosity = Verbose
-    , shakeColor = True
+options =
+  shakeOptions
+    { shakeVerbosity = Verbose,
+      shakeColor = True
     }
-
 
 elmDist :: FilePath
 elmDist = "dist/js/elm.js"
 
-
 it :: FilePath
 it = "integration-tests"
-
 
 chromedriver :: FilePath
 chromedriver = it </> "chromedriver"
 
-
 chromedriverZip :: FilePath
 chromedriverZip = it </> "chromedriver_linux64.zip"
 
-
 testApp :: FilePath
 testApp = it </> "target" </> "testApp.html"
-
 
 minify :: String
 minify = "minify"
